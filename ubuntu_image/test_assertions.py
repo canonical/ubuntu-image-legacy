00289cc7d67055c5fee4c796e765a9a7cfa735bf
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-05-18 00:53:51 +0200

    Add initial (rough) code for assertions
    This patch builds upon the checkbox-derived rfc822 parser to parse
    assertions (without support for body or signature yet). It is sufficient
    to parse hand-made model assertions that are required to bootstrap the
    rest of the process.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ ubuntu_image/test_assertions.py
@@ -0,0 +1,34 @@
+import unittest
+
+from ubuntu_image.assertions import ModelAssertion
+
+fake_model_assertion = """
+type: model
+authority-id: nobody
+series: 16
+brand-id: zygoon
+model: kvm-demo
+os: ubuntu-core
+architecture: amd64 
+kernel: canonical-pc-linux
+gadget: canonical-pc
+required-snaps: links
+# preinstalled-snaps: a, b
+# prefetched-snaps: docker
+body-size: 0
+"""
+# TODO: add a fake body and signature once the parser understands it
+
+class ModelAssertionTest(unittest.TestCase):
+
+    def test_smoke(self):
+        model = ModelAssertion.from_string(fake_model_assertion)
+        self.assertEqual(model.type, "model")
+        self.assertEqual(model.authority_id, "nobody")
+        self.assertEqual(model.series, "16")
+        self.assertEqual(model.brand_id, "zygoon")
+        self.assertEqual(model.os, "ubuntu-core")
+        self.assertEqual(model.architecture, "amd64")
+        self.assertEqual(model.kernel, "canonical-pc-linux")
+        self.assertEqual(model.gadget, "canonical-pc")
+        self.assertEqual(model.required_snaps, "links")
