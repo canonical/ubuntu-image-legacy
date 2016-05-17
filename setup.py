15f5496d06df6b7421f42d2a0578646702d35a64
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-05-17 23:10:32 +0200

    Initial code scaffolding
    This patch adds some basic scaffolding for the code:
     - setup.py
     - ubuntu-image executable
     - ubuntu_image package
     - dummy test
     - travis.yaml for travis-ci integration
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='ubuntu-image',
+    version='0.1',
+    description='construct snappy images out of a model assertion',
+    author_email='snapcraft@lists.ubuntu.com',
+    url='https://github.com/CanonicalLtd/ubuntu-image',
+    packages=['ubuntu_image'],
+    scripts=['ubuntu-image'],
+    install_requires=[],
+    test_suite='ubuntu_image',
+    license='GPLv3',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Software Distribution',
+    ),
+)
