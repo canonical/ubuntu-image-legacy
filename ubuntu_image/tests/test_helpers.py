98f9e6ba39bc8922e4798ef433a97a1e2ab662fc
Barry Warsaw <barry@python.org>
2016-06-24 19:39:59 -0400

    100% coverage.

---
+++ ubuntu_image/tests/test_helpers.py
@@ -0,0 +1,16 @@
+"""Test the helpers."""
+
+
+from ubuntu_image.helpers import GiB, MiB, as_size
+from unittest import TestCase
+
+
+class TestHelpers(TestCase):
+    def test_m(self):
+        self.assertEqual(as_size('25M'), MiB(25))
+
+    def test_g(self):
+        self.assertEqual(as_size('30G'), GiB(30))
+
+    def test_bytes(self):
+        self.assertEqual(as_size('801'), 801)
