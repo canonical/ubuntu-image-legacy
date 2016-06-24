7f878cbb7b2aa32764d6e22c28b86c78cb81e713
Barry Warsaw <barry@python.org>
2016-06-24 19:39:59 -0400

    100% coverage.

--- ubuntu_image/phases.py
+++
@@ -1,9 +0,0 @@
-"""Bootstrap phases."""
-
-
-def phase1(unpack_dir, channel, model):
-    # We don't actually have to parse the model assertion.  We just have to
-    # pass it to `snap bootstrap`.
-    print('gadget-unpack-dir:', unpack_dir)
-    print('channel:', channel)
-    print('model assertion:', model)
