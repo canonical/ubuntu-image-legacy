5bc5089c79deb01aa1abf079da2e04bb40214128
Barry Warsaw <barry@python.org>
2016-08-25 16:47:04 -0400

    Import order.

--- ubuntu_image/builder.py
+++ ubuntu_image/builder.py
@@ -10,7 +10,7 @@ from operator import attrgetter
 from tempfile import TemporaryDirectory
 from ubuntu_image.helpers import GiB, MiB, run, snap
 from ubuntu_image.image import Image
-from ubuntu_image.parser import parse as parse_yaml, FileSystemType
+from ubuntu_image.parser import FileSystemType, parse as parse_yaml
 from ubuntu_image.state import State
 
 
