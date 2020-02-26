bbc8a097428ce6b966aafbe964a84ca126b2b9a9
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2020-02-26 16:28:17 +0100

    Re-add py37 to tox.ini but work-around it for ADT tests.

--- debian/tests/unittests.sh
+++ debian/tests/unittests.sh
@@ -2,22 +2,14 @@
 
 export UBUNTU_IMAGE_CODENAME=`lsb_release -cs`
 
-# Xenial is special; all the others are share a configuration.
-
-if [ "$UBUNTU_IMAGE_CODENAME" != 'xenial' ]
-then
-    export UBUNTU_IMAGE_CODENAME="devel"
-fi
-
 # Normally we want to attempt running the tests for all envs, but currently
 # in focal we support both 3.7 and 3.8 while pyparted is built and available
 # only against 3.8.  So we skip py37 for it.
 
-ENVLIST="py35-cov,py36-cov,py37-cov,py38-cov"
-
+ENVLIST="py35-nocov,py36-nocov,py37-nocov,py38-nocov"
 if [ "$UBUNTU_IMAGE_CODENAME" = 'focal' ]
 then
-    ENVLIST="py35-cov,py36-cov,py38-cov"
+    ENVLIST="py35-nocov,py36-nocov,py38-nocov"
 fi
 
 tox -e $ENVLIST
