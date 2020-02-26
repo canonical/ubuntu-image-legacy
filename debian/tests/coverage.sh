f9be13e6ecf50926647748460225c398d2af0693
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2020-02-26 21:54:48 +0100

    Fix some stupid mistakes in the previous commit.

--- debian/tests/coverage.sh
+++ debian/tests/coverage.sh
@@ -2,13 +2,6 @@
 
 export UBUNTU_IMAGE_CODENAME=`lsb_release -cs`
 
-# Xenial is special; all the others are share a configuration.
-
-if [ "$UBUNTU_IMAGE_CODENAME" != 'xenial' ]
-then
-    export UBUNTU_IMAGE_CODENAME="devel"
-fi
-
 # Normally we want to attempt running the tests for all envs, but currently
 # in focal we support both 3.7 and 3.8 while pyparted is built and available
 # only against 3.8.  So we skip py37 for it.
@@ -18,6 +11,13 @@ ENVLIST="py35-cov,py36-cov,py37-cov,py38-cov"
 if [ "$UBUNTU_IMAGE_CODENAME" = 'focal' ]
 then
     ENVLIST="py35-cov,py36-cov,py38-cov"
+fi
+
+# Xenial is special; all the others are share a configuration.
+
+if [ "$UBUNTU_IMAGE_CODENAME" != 'xenial' ]
+then
+    export UBUNTU_IMAGE_CODENAME="devel"
 fi
 
 tox -e $ENVLIST
